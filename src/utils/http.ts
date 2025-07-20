76a227c7c9f64240a91e013cb5ee89ac4852e97c
shanmengqi <shanmengqi@gmail.com>
2025-07-21 01:22:21 +0800

    feat: 请求和上传文件拦截器

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,3 +1,5 @@
+import { useMemberStore } from '@/stores'
+
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 
 // 添加拦截器
@@ -7,6 +9,15 @@ const httpInterceptor = {
       options.url = baseURL + options.url
     }
     options.timeout = 10000
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+    }
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
     console.log(options)
   },
 }
