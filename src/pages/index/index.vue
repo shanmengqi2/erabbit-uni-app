c3f337a0d2d64e661aa5eae1e78f70ef0cae6c9a
shanmengqi <shanmengqi@gmail.com>
2025-07-22 19:25:12 +0800

    feat(home): add category panel component and page background
    Add new CategoryPanel component to display home categories and set page background color

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,6 +5,7 @@ import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
@@ -20,7 +21,12 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
+</style>
