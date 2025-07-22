afa3a3116e07a1bfc41c216d7fe3c1e01b9a5c54
shanmengqi <shanmengqi@gmail.com>
2025-07-23 02:53:20 +0800

    feat(home): add category panel with dynamic data
    - Add CategoryItem type definition in home.d.ts
    - Implement getHomeCategoryAPI service to fetch category data
    - Update CategoryPanel component to display dynamic category data
    - Fetch and pass category data in index page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,7 +4,8 @@ import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import { getHomeCategoryAPI } from '@/services/home'
 import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
@@ -13,15 +14,22 @@ const getHomeBannerData = async () => {
   bannerList.value = res.result
 }
 
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
-  <CategoryPanel />
+  <CategoryPanel :list="categoryList" />
   <view class="index">index</view>
 </template>
 
