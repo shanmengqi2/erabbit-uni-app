19728e80dd379295bdbc0c3d4387ccf911691f4a
shanmengqi <shanmengqi@gmail.com>
2025-07-29 07:09:26 +0800

    feat(category): implement dynamic subcategory display
    Add watch for activeIndex to update subCategoryList when category changes
    Update template to render actual subcategory data instead of placeholders

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -2,9 +2,9 @@
 import { getHomeBannerAPI } from '@/services/home'
 import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { getCategoryTopAPI } from '@/services/category'
-import type { CategoryTopItem } from '@/types/category'
+import type { CategoryTopItem, CategoryChildItem } from '@/types/category'
 
 const activeIndex = ref(0)
 
@@ -26,12 +26,22 @@ const categoryList = ref<CategoryTopItem[]>([])
 const getCategoryTopData = async () => {
   const res = await getCategoryTopAPI()
   categoryList.value = res.result
+  // 手动设置初始的子分类数据
+  if (categoryList.value.length > 0) {
+    subCategoryList.value = categoryList.value[0].children
+  }
 }
 
 // 页面加载
 onLoad(() => {
   getBannerData()
   getCategoryTopData()
+})
+
+// 提取当前二级分类数据
+const subCategoryList = ref<CategoryChildItem[]>([])
+watch(activeIndex, (newVal) => {
+  subCategoryList.value = categoryList.value[newVal].children
 })
 </script>
 
@@ -62,27 +72,24 @@ onLoad(() => {
         <!-- 焦点图 -->
         <XtxSwiper class="banner" :list="bannerList" />
         <!-- 内容区域 -->
-        <view class="panel" v-for="item in 3" :key="item">
+        <view class="panel" v-for="item in subCategoryList" :key="item.id">
           <view class="title">
-            <text class="name">宠物用品</text>
+            <text class="name">{{ item.name }}</text>
             <navigator class="more" hover-class="none">全部</navigator>
           </view>
           <view class="section">
             <navigator
-              v-for="goods in 4"
-              :key="goods"
+              v-for="goods in item.goods"
+              :key="goods.id"
               class="goods"
               hover-class="none"
-              :url="`/pages/goods/goods?id=`"
+              :url="`/pages/goods/goods?id=${goods.id}`"
             >
-              <image
-                class="image"
-                src="https://yanxuan-item.nosdn.127.net/674ec7a88de58a026304983dd049ea69.jpg"
-              ></image>
-              <view class="name ellipsis">木天蓼逗猫棍</view>
+              <image class="image" :src="goods.picture"></image>
+              <view class="name ellipsis">{{ goods.name }}</view>
               <view class="price">
                 <text class="symbol">¥</text>
-                <text class="number">16.00</text>
+                <text class="number">{{ goods.price }}</text>
               </view>
             </navigator>
           </view>
